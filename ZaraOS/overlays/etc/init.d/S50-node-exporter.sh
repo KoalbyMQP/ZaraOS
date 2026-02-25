09c059814385d736a33cb4c947bf85b862ef17e5
Jacob Molnia <jrmolnia@wpi.edu>
2026-02-25 09:11:07 -0500

    added node exporter with autosetup

--- ZaraOS/overlays/etc/init.d/S50-node-exporter.sh
+++ ZaraOS/overlays/etc/init.d/S50-node-exporter.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
 case "$1" in
-    start) node_exporter --web.listen-address=":9100" &;;
-    stop)  killall node_exporter 2>/dev/null || true;;
-    *)     echo "Usage: $0 {start|stop}"; exit 1;;
+    start)
+        echo "Starting node_exporter..."
+        node_exporter --web.listen-address=":9100" < /dev/null &
+        ;;
+    stop)
+        killall node_exporter 2>/dev/null || true
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
 esac
